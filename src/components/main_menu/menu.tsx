3c6868ac003fe7dc635bc7f272bbab86390043b8
vwency <veniaminshp@gmail.com>
2025-09-01 22:16:03 +0600

    feat: menu

--- src/components/main_menu/menu.tsx
+++ src/components/main_menu/menu.tsx
@@ -3,9 +3,19 @@ import '@/styles/menu/index.scss';
 
 const menuContent = (
     <div className="menu_wrapper">
-        <div className="menu__logo"><div className="menu__logo__text">
-            Serendale</div></div>
-        <div className="menu__options">qwe</div>
+        <div className="menu__logo">
+            <div className="menu__logo__text">
+                Serendale
+            </div>
+        </div>
+        <div className="menu__options">
+            <ul className="list-inline">
+                <li className="list-inline__item">Smart Contracts</li>
+                <li className="list-inline__item">Services</li>
+                <li className="list-inline__item">Solutions</li>
+                <li className="list-inline__item">Whitepaper</li>
+            </ul>
+        </div>
         <div className="menu__refferal">qwe</div>
     </div>
 );
