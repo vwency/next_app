5a63092ce6bedbd202a3b3dbd046129a984cb26e
vwency <veniaminshp@gmail.com>
2025-09-04 00:43:35 +0600

    Updates

---
+++ _next/static/chunks/app/layout-de6705833428cfa0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{8923:()=>{}},_=>{_.O(0,[128,251,358],()=>_(_.s=8923)),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/_app-80eaed5c026b8afb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4406:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(2113)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4406),n(7733))),_N_E=_.O()}]);
