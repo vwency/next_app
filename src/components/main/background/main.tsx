bee8d596831a442c1eb6dd008179bc5b5b7fa330
vwency <veniaminshp@gmail.com>
2025-09-03 18:37:13 +0600

    feat: background added

---
+++ src/components/main/background/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainBackground = () => {
+  return (
+    <div className="main__background__wrapper">
+      <div className="qwe">
+        <img src="assets/backgorund_m.png" alt="Background" />
+      </div>
+    </div>
+  )
+}
+
+export default MainBackground
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,12 +4,14 @@ import MainTextHead from './text_head/main'
 import MainText from './text_main/main'
 import MainFooterText from './text_footer/main'
 import MainButtons from './buttons/main'
+import MainBackground from './background/main'
 const MainContent = (
   <div>
     <MainTextHead />
     <MainText />
     <MainFooterText />
     <MainButtons />
+    <MainBackground />
   </div>
 )
 
