eccb185c41c4e9761a25a21f6dec78e95a1a52b8
vwency <veniaminshp@gmail.com>
2025-09-08 14:04:59 +0600

    chore: remove background

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,16 +4,14 @@ import MainTextHead from './text_head/main'
 import MainText from './text_main/main'
 import MainFooterText from './text_footer/main'
 import MainButtons from './buttons/main'
-import MainBackground from './background/main'
 import MainSlider from './slider/main'
 const MainContent = (
-  <div className="main__layout">
+  <div className="main__layout no-select">
     <MainTextHead />
     <MainText />
     <MainFooterText />
     <MainSlider />
     <MainButtons />
-    <MainBackground />
   </div>
 )
 
