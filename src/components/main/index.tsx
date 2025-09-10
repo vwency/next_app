73d34f2ee4bebf2e9b6c94d9244717fdce9d3856
vwency <veniaminshp@gmail.com>
2025-09-10 18:55:14 +0600

    chore(modal): android adaptive image height

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,10 +4,12 @@ import MainTextHead from './text_head/main'
 import MainText from './text_main/main'
 import MainButtons from './buttons/main'
 import MainSlider from './slider/main'
+import MainFooterText from './text_footer/main'
 const MainContent = (
   <div className="main__layout no-select">
     <MainTextHead />
     <MainText />
+    <MainFooterText />
     <MainSlider />
     <MainButtons />
   </div>
