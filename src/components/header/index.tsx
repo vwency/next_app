6334aa648ada9c1e6228cbff8a5df826eea94f0b
vwency <veniaminshp@gmail.com>
2025-09-11 15:23:53 +0600

    fix: hide menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect, useRef } from 'react'
 import MainMenu from './menu/menu'
 import '@/styles/header/index.scss'
 
-interface HeaderLayoutProps {
-  contentRef: React.RefObject<HTMLDivElement | null>
-}
+const SCROLL_HIDE_THRESHOLD = 1
 
-const HeaderLayout: React.FC<HeaderLayoutProps> = ({ contentRef }) => {
+const HeaderLayout: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true)
+  const lastScrollY = useRef(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      const diff = currentScrollY - lastScrollY.current
+
+      let newVisibility = isVisible
+
+      if (currentScrollY <= SCROLL_HIDE_THRESHOLD) {
+        newVisibility = true
+      } else if (diff > 0) {
+        newVisibility = false
+      } else if (diff < 0) {
+        newVisibility = true
+      }
+
+      if (newVisibility !== isVisible) {
+        setIsVisible(newVisibility)
+      }
+
+      lastScrollY.current = currentScrollY
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isVisible])
+
   return (
-    <div className="header_wrapper no-select">
-      <MainMenu contentRef={contentRef} />
+    <div
+      className={`header_wrapper no-select ${isVisible ? 'visible' : 'hidden'}`}
+    >
+      <MainMenu />
     </div>
   )
 }
 
-export default HeaderLayout
+export default React.memo(HeaderLayout)
